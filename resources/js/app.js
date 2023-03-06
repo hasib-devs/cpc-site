09381a62d095c3d0da812c2ab53d178ad88b9e0d
Hasibur Rahman <hasib.webdev@gmail.com>
2023-03-06 21:51:14 +0600

    removed comments

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import '../css/app.css'
+
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+
+export default function render(page) {
+  return createInertiaApp({
+    page,
+    render: ReactDOMServer.renderToString,
+    resolve: (name) => require(`./Pages/${name}`),
+    setup: ({ App, props }) => <App {...props} />,
+  })
+}
