9985fe75d587fa93a930dbccb22659b10c093419
Hasibur Rahman <hasib.webdev@gmail.com>
2023-03-06 23:31:34 +0600

    added types

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 
+import { InertiaProgress } from '@inertiajs/progress'
+
+import '../css/app.scss'
+
+InertiaProgress.init()
+
 export default function render(page) {
   return createInertiaApp({
     page,
