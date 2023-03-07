431db68b2633baa390053b56316f1c4d8d726f66
Hasibur Rahman <hasib.webdev@gmail.com>
2023-03-07 21:00:17 +0600

    Added sementic Ui

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 const Home = () => {
   return (
     <div>
       <p className="">Home page</p>
+      <Button>Click Here</Button>
     </div>
   )
 }
